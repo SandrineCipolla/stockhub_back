c325712f182da9082a0cf458b44987628f084655
SandrineCipolla <sandrine.cipolla@gmail.com>
2024-11-18 11:31:26 +0100

    added log on database configuration.

--- src/configurationDb.ts
+++ src/configurationDb.ts
@@ -1,8 +1,6 @@
 //Config database
 
-import dotenv from "dotenv";
-
-dotenv.config();
+import { rootDatabase } from "./Utils/logger";
 
 const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
@@ -10,10 +8,18 @@ const dbPassword = process.env.DB_PASSWORD;
 const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3307;
 const dbDatabase = process.env.DB_DATABASE;
 
+rootDatabase.info(
+  "Database configuration {host}, {user}, {port}, {database}",
+  dbHost,
+  dbUser,
+  dbPort,
+  dbDatabase
+);
+
 export const connectionOptions = {
-    host: dbHost,
-    user: dbUser,
-    password: dbPassword,
-    port: dbPort,
-    database: dbDatabase,
+  host: dbHost,
+  user: dbUser,
+  password: dbPassword,
+  port: dbPort,
+  database: dbDatabase,
 };
