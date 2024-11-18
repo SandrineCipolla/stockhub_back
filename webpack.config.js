4559c7e4117de9a7044ccafa6dd09053956f9d1d
SandrineCipolla <sandrine.cipolla@gmail.com>
2024-11-18 11:04:30 +0100

    changing http port source.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 //    externals: [nodeExternals()], // Exclut `node_modules` du bundle
-
 module.exports = {
-    entry: './src/index.ts', // Point d'entrée de votre application
-    target: 'node', // Spécifie que le bundle est pour Node.js, pas pour le navigateur
-
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'server.js',
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    mode: 'production', // Peut être changé à 'development' pour un build non minifié
+  entry: "./src/index.ts",
+  target: "node",
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "production",
 };
