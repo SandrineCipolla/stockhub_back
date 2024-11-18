d9ca23a779a3f2dcdf8faa47197223a82dd41b7b
SandrineCipolla <sandrine.cipolla@gmail.com>
2024-11-18 11:20:58 +0100

    using webpack to create a bundle for the server.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const nodeExternals = require("webpack-node-externals");
 module.exports = {
   entry: "./src/index.ts",
   target: "node",
-  externals: [nodeExternals()],
+  // externals: [nodeExternals()],
   module: {
     rules: [
       {
