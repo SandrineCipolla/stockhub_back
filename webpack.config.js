ab9604fc7e25b60dcab1f0c2c2e30d9e5f6af6c0
SancCi <100938864+SandrineCipolla@users.noreply.github.com>
2025-09-14 12:14:07 +0200

    change prisma generation process with webpack. using native in binary target.

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,26 @@ const nodeExternals = require("webpack-node-externals");
 
 //    externals: [nodeExternals()], // Exclut `node_modules` du bundle
 module.exports = {
-  entry: "./src/index.ts",
-  target: "node",
-  // externals: [nodeExternals()],
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  mode: "production",
+    entry: "./src/index.ts",
+    target: "node",
+    externals: [nodeExternals({
+        allowlist: [/^(?!@prisma\/client)/] // Exclure spécifiquement @prisma/client
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    mode: "production",
 };
